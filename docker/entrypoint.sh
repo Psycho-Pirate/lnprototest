555981815c892fc5503bce52ae41140042165870
psycho-pirate <prakharsaxena.civ18@iitbhu.ac.in>
2023-08-03 16:20:36 +0530

    workflow added

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,13 +3,6 @@ cd lnprototest || exit
 
 for i in range{0..5};
 do
-  if make check PYTEST_ARGS='--runner=lnprototest.clightning.Runner -n8 --dist=loadfile --log-cli-level=DEBUG'; then
-    echo "iteration $i succeeded"
-  else
-    echo "iteration $i failed"
-    exit 1
-  fi
-do
   if make check PYTEST_ARGS='--runner=lnprototest.ldk.Runner -n8 --dist=loadfile --log-cli-level=DEBUG'; then
     echo "iteration $i succeeded"
   else
