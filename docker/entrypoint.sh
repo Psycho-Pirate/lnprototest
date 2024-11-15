d5228d3bc29f7fbb873fe274d79988bd1e8846d4
Psycho-Pirate <prakharrsaxena@gmail.com>
2024-11-15 19:33:33 +0530

    ubuntu version fixed

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 cd lnprototest || exit
 
-for i in range{0..1};
+for i in range{0..5};
 do
   if make check PYTEST_ARGS='--runner=lnprototest.ldk.Runner -n8 --dist=loadfile --log-cli-level=DEBUG -vvv -s'; then
     echo "iteration $i succeeded"
